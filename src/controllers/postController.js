ff6a3f186a53dcd6377aee77d126a2505ce9eb5e
Corey Russ <corey.j.russ@gmail.com>
2018-07-11 10:01:15 -0700

    Post form view working

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -2,4 +2,9 @@ const postQueries = require("../db/queries.posts.js");
 
 module.exports = {
 
+  new(req, res, next){
+    res.render("posts/new", {categoryId: req.params.categoryId,
+    sectionId: req.params.sectionId, topicId: req.params.topicId});
+  }
+
 }
--- src/routes/posts.js
+++ src/routes/posts.js
@@ -2,4 +2,6 @@ const express = require("express");
 const router = express.Router();
 const postController = require("../controllers/postController")
 
+router.get("/categories/:categoryId/sections/:sectionId/topics/:topicId/posts/new", postController.new);
+
 module.exports = router;
