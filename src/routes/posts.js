868d784a8a4aae363549a0a906259b53b4711043
Corey Russ <corey.j.russ@gmail.com>
2018-07-11 17:35:35 -0700

    google api added, request needs to be added and buttons not working

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -3,5 +3,5 @@ const router = express.Router();
 const postController = require("../controllers/postController")
 
 router.get("/categories/:categoryId/sections/:sectionId/topics/:topicId/posts/new", postController.new);
-
+//POST https://www.googleapis.com/upload/youtube/v3/videos
 module.exports = router;
